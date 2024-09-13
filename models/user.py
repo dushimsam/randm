489493d1b3f7ac71c5e557b230c2fb959d110adf
Samuel Dushimimana <66276301+dushimsam@users.noreply.github.com>
2024-09-13 08:03:08 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module contains the User class (Blueprint for creating user objects).
+This module defines the `User` class, which serves as a blueprint for creating
+user objects within the application.
 """
 
 from models.base_model import BaseModel
@@ -8,14 +9,22 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """
-    This is the user class.
+    Represents a user within the application.
+
+    The `User` class extends `BaseModel` and is used to manage and store information
+    related to individual users. This class facilitates the management of user-specific
+    details such as authentication and personal information.
 
     Attributes:
-        email (str): The email address of the user.
-        password (str): The password of the user.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
+        email (str): The email address associated with the user account. Used for 
+                     user identification and communication.
+        password (str): The password for the user account, typically stored in a
+                        hashed format for security.
+        first_name (str): The user's first name, used for personal identification.
+        last_name (str): The user's last name, used for personal identification and 
+                         formal communication.
     """
+    
     email = ""
     password = ""
     first_name = ""
@@ -23,11 +32,14 @@ class User(BaseModel):
 
     def __init__(self, *args, **kwargs):
         """
-        Initialize public instance attributes.
+        Initializes a new `User` instance.
+
+        This method sets up the user attributes either from provided keyword arguments 
+        (kwargs) or by using default values. It ensures that the user object is correctly
+        initialized with all necessary attributes by inheriting from the `BaseModel` class.
+
+        Args:
+            *args: Variable length argument list (not used in this implementation).
+            **kwargs: Dictionary of keyword arguments to initialize the user attributes.
         """
         super().__init__(*args, **kwargs)
-
-        # self.email = User.email
-        # self.password = User.password
-        # self.first_name = User.first_name
-        # self.last_name = User.last_name
